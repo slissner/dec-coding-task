1d2dd35ebb54aa58d5d807fa8de28aa22f6e2f98
Samuel Lissner <samuel.lissner@protonmail.com>
2024-09-10 14:39:49 +0200

    Fix error when logging out

--- client/src/components/layout/LoggedInLayout.vue
+++ client/src/components/layout/LoggedInLayout.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-import { useSessionStore } from '@/stores/session-store'
 import { AuthService, authServiceKey } from '@/services/auth-service'
 import { injectStrict } from '@/services/inject-service'
+import { ref } from 'vue'
 
 const authService: AuthService = injectStrict(authServiceKey)
 
+const shouldDestroyDom = ref(false)
+
 function userClickedLogoutButton() {
+  // Clear DOM in order to avoid errors for inconsistent state when user logs out
+  // e.g. "session-store.ts:12 Uncaught (in promise) Error: Cannot retrieve logged in session if the session is not in logged in state"
+  shouldDestroyDom.value = true
+
   authService.logoutUser()
 }
 </script>
@@ -25,5 +31,7 @@ function userClickedLogoutButton() {
     </ul>
   </nav>
 
-  <slot></slot>
+  <div v-if="!shouldDestroyDom">
+    <slot></slot>
+  </div>
 </template>
